bf1f029ba493800d0d2c6b3f9761907f6fd49d98
juanfaldana20 <98820412+juanfaldana20@users.noreply.github.com>
2023-02-28 16:14:16 -0500

    challenge

--- Challenge20/src/main.jsx
+++ Challenge20/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+function App() {
+  return <h1> HOLA MUNDO</h1>
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
