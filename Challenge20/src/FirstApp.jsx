2097df4844bb0f9882e1c7b8763158765e436ad4
juanfaldana20 <98820412+juanfaldana20@users.noreply.github.com>
2023-02-28 16:24:14 -0500

    CHallenge1
    Challenge1

---
+++ Challenge20/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const title ='First App';
+
+    const FirstApp = ( ) => {
+        return (
+        <> <hl> { title } </hl>
+            <span> 10 </span>
+        </>
+    )
+}
+export default FirstApp
--- Challenge20/src/main.jsx
+++ Challenge20/src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FirstApp from './FirstApp'
 import './index.css'
 
-function App() {
-  return <h1> HOLA MUNDO</h1>
-}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FirstApp/>
   </React.StrictMode>,
 )
