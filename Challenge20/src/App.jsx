29ed456b399575ad6f778e74024f910b4536e3cf
juanfaldana20 <98820412+juanfaldana20@users.noreply.github.com>
2023-04-08 15:05:02 -0500

    multiple Custom hooks

--- Challenge20/src/App.jsx
+++ Challenge20/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import ComponentApp from "./ComponentApp"
+import { MultipleCustomHooks } from './MultipleCustomHooks'
 
 const App=() =>{
 
@@ -7,7 +8,7 @@ const App=() =>{
 
   return (
     <div>
-      <ComponentApp/>
+      <MultipleCustomHooks/>
 
     </div>
 
